85a87a26facdf337262062d5b13caf9fa0a8df20
skysee6126 <chooshinhye@gmail.com>
2021-04-05 14:09:29 +0900

    added info

--- indeed.py
+++ indeed.py
@@ -17,6 +17,20 @@ def extract_indeed_pages():
     max_page = pages[-1]
     return max_page
 
+def extract_indeed_job(html):
+        title =html.find("h2", {"class": "title"}).find("a")["title"]
+        company = html.find("span", {"class": "company"})
+        company_anchor = company.find("a")
+        if company_anchor is not None:
+            company = (str(company_anchor.string))
+        else:
+            company = (str(company.string))
+        company = company.strip()
+        location = html.find("div", {"class": "recJobLoc"})["data-rc-loc"]
+        job_id = html.find("h2", {"class":"title"}).find("a")["href"]
+        return {'title': title, 'company': company, 'location': location, 'link': f"https://kr.indeed.com{job_id}"}
+
+
 def extract_indeed_jobs(last_page):
     jobs = []
     for page in range(last_page):
@@ -24,6 +38,6 @@ def extract_indeed_jobs(last_page):
         soup = BeautifulSoup(result.text,"html.parser")
         results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
         for result in results:
-            title =result.find("h2", {"class": "title"}).find("a")["title"]
-            print(title)
+            job = extract_indeed_job(result)
+            jobs.append(job)
     return jobs
